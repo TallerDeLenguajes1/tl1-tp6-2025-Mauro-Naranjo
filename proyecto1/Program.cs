e9ce0d49b122dd98284b89e06366a3172f9bdec5
mauro <mnaranjososa@gmail.com>
2025-06-07 00:47:05 -0300

    ejercicio 1 realizado

---
+++ proyecto1/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int a;
+int b;
+a = 10;
+b = a;
+Console.WriteLine("valor de a:" + a);
+Console.WriteLine("valor de b:" + b);
+
+string? numeroString;
+int numero;
+Console.WriteLine("ingrese un numero");
+numeroString = Console.ReadLine();
+bool resultado = int.TryParse(numeroString, out numero);
+
+int Digito;
+string numInver = "";
+if (resultado && numero > 0)
+{
+    do
+    {
+        Digito = numero % 10;
+        numero = numero / 10;
+        numInver = numInver + Digito;
+    } while (numero > 0);  
+}
+
+Console.WriteLine("El numero invertido es: " + numInver);
