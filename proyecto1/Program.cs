32cdc529cb5cbfff51b550b2e37b66d786c15731
mauro <mnaranjososa@gmail.com>
2025-06-23 19:57:46 -0300

    calculadora punto 2

--- proyecto1/Program.cs
+++ proyecto1/Program.cs
@@ -27,3 +27,56 @@ if (resultado && numero > 0)
 }
 
 Console.WriteLine("El numero invertido es: " + numInver);
+
+//ejercicio 2
+bool continuar = true;
+
+while (continuar)
+{
+    Console.WriteLine("Ingrese la operacion a realizar");
+    Console.WriteLine("1. Sumar");
+    Console.WriteLine("2. Restar");
+    Console.WriteLine("3. Multiplicar");
+    Console.WriteLine("4. Dividir");
+
+    string? opcion = Console.ReadLine();
+
+    Console.WriteLine("Ingrese el primer numero a operar: ");
+    string? numero1 = Console.ReadLine();
+    Console.WriteLine("Ingrese el segundo numero a operar: ");
+    string? numero2 = Console.ReadLine();
+
+
+    if (float.TryParse(numero1, out float num1) && float.TryParse(numero2, out float num2))
+    {
+        switch (opcion)
+        {
+            case "1":
+                Console.WriteLine("El resultado es: " + (num1 + num2));
+                break;
+            case "2":
+                Console.WriteLine("El resultado es: " + (num1 - num2));
+                break;
+            case "3":
+                Console.WriteLine("El resultado es: " + (num1 * num2));
+                break;
+            case "4":
+                Console.WriteLine("El resultado es: " + (num1 / num2));
+                break;
+            default:
+                Console.WriteLine("La opcion ingresada es incorrecta");
+                break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Los numeros ingresados son invalidos");
+    }
+
+    Console.WriteLine("Desea realizar otra operacion? (s/n): ");
+    string? respuesta = Console.ReadLine();
+    if (respuesta?.ToLower() != "s")
+    {
+        continuar = false;
+    }
+}
